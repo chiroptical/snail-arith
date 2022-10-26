5d26ab8fcefb80b3cfc94ac386f026841eeb061a
Barry Moore <chiroptical@gmail.com>
2022-10-26 18:56:55 -0400

    Nix clean-up

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    snail-shell.url = "github:chiroptical/snail-shell";
+    snail-shell-src.url = "github:chiroptical/snail-shell";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    snail-shell,
+    snail-shell-src,
     ...
   }:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
@@ -19,13 +19,15 @@
         inherit system;
         config.allowBroken = true;
       };
-      snail-arith = pkgs.callPackage ./snail-arith.nix {
-        snail-shell = snail-shell.packages.${system}.snail-shell;
+      snail-shell = snail-shell-src.packages.${system}.snail-shell;
+      snail-arith = import ./snail-arith.nix {
+        inherit pkgs;
+        inherit snail-shell;
       };
     in {
       devShell = import ./shell.nix {
         inherit pkgs;
-        snail-shell = snail-shell.packages.${system}.snail-shell;
+        inherit snail-shell;
       };
       defaultPackage = snail-arith;
       packages = flake-utils.lib.flattenTree {
